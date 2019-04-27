be901369be549c4abd9871b035e819a14a2b9738
rodrigo cunha <rodrigo.root.rj@gmail.com>
2019-04-27 18:33:22 -0300

    002.TransformaStringDentroDoArquivo

--- Tartaro/Shell.soul/templates.d/commands.d/sed.soul/002.TransformaStringDentroDoArquivo/main.sh
+++ Tartaro/Shell.soul/templates.d/commands.d/sed.soul/002.TransformaStringDentroDoArquivo/main.sh
@@ -2,4 +2,14 @@
 #Autor:Rodrigo Cunha
 #Objetuvo: Alterar Arquivos
 source ./Variaveis/V0
-
+source ./Variaveis/V1
+echo "Procedimentos[A]
+A:Remover Linhas em Branco do Final do Arquivo.
+B:Extrair primeiro e ultimo nome de usuario.
+C:Remover ultimo Caractere da string
+D:Substituir \".\" por \",\".
+"
+read _lv1
+export _lv1="$_lv1"
+source ./Modulos/M0
+echo Core: $_lv1
